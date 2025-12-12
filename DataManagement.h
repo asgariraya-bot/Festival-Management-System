b9362441a27cd3e05cd77f7b0cda7c4719e42ff8
Raya Asgari <asgariraya@gmail.com>
2025-12-12 14:50:41 +0330

    add DataManagement.h

---
+++ DataManagement.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <vector>
+#include <algorithm>   
+#include <fstream>      
+#include <string>      
+#include <exception>     
+#include <utility>   
+
+#include "AutoIncremental.h"
+using namespace std;
+
+// Manage objects with unique ID, support save, delete, CSV export
+template<typename datatype>
+class DataManagement 
+{
+protected:
+    AutoIncremental<datatype> id;   // unique ID
+    static vector<datatype> all;    // all objects
+
+public:
+    // Create a new object
+    template<typename... Args>
+    static datatype& create(Args&&... args) 
+    {
+        datatype obj(forward<Args>(args)...);
+
+        if (exists(obj))
+            throw UniqueConstraintFailed();
+
+        obj.id.assignNext();
+        all.push_back(obj);
+
+        return all.back();
+    }
+
+    // Get object by ID
+    static datatype& getById(int64_t id) 
+    {
+        for (auto& item : all)
+            if ((int64_t)item.id == id)
+                return item;
+
+        throw ItemNotFound();
+    }
+
+    // Check if object exists
+    static bool exists(const datatype& value) 
+    {
+        return any_of(all.begin(), all.end(),
+            [&](const datatype& x) { return x == value; });
+    }
+
+    // Delete object at index
+    static bool delAt(int index) 
+    {
+        if (index < 0 || index >= all.size()) return false;
+        all.erase(all.begin() + index);
+        return true;
+    }
+
+    // Save all objects to CSV
+    static void saveCSV(const string& filename) 
+    {
+        ofstream out(filename);
+        for (auto& item : all) out << item.toCSVLine() << "\n";
+    }
+
+    // Get ID of this object
+    int64_t getId() const { return (int64_t)id; }
+};
+
+template<typename datatype>
+vector<datatype> DataManagement<datatype>::all;
